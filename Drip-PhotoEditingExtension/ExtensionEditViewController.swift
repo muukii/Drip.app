096e44fd47eb7fbbe803bdcc3ba08e8f365c3ae1
Muukii <muukii.app@gmail.com>
2022-01-05 04:15:43 +0900

    Setup extension

---
+++ Drip-PhotoEditingExtension/ExtensionEditViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoEditingViewController.swift
+//  Drip-PhotoEditingExtension
+//
+//  Created by Muukii on 2022/01/04.
+//
+
+import UIKit
+import Photos
+import PhotosUI
+import DripCore
+
+final class PrimaryViewController: ExtensionEditViewController {
+
+}
