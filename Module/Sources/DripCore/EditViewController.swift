1ef8b086a7dd9b7f478ee781f1c6d68b95a5e58e
Muukii <muukii.app@gmail.com>
2022-01-06 03:03:42 +0900

    :evergreen_tree: Update

--- Module/Sources/DripCore/EditViewController.swift
+++ Module/Sources/DripCore/EditViewController.swift
@@ -4,18 +4,27 @@ import CompositionKit
 import FluidInterfaceKit
 import MondrianLayout
 import UIKit
+import Verge
 
 final class EditViewController: CodeBasedViewController, ViewControllerFluidContentType {
 
   private let previewImageView: ImagePreviewView
+  private let presetSelectionView: DynamicContentListView<PreviewFilterPreset>
+  private let editingStack: EditingStack
+
+  private var subscriptions = VergeAnyCancellables()
 
   init(
     editingStack: EditingStack
   ) {
 
+    self.editingStack = editingStack
     self.previewImageView = .init(editingStack: editingStack)
-
-    editingStack.state.loadedState?.previewFilterPresets
+    self.presetSelectionView = .init(
+      scrollDirection: .horizontal,
+      spacing: 8,
+      contentInsetAdjustmentBehavior: .never
+    )
 
     super.init()
   }
@@ -24,10 +33,6 @@ final class EditViewController: CodeBasedViewController, ViewControllerFluidCont
     super.viewDidLoad()
 
     view.backgroundColor = .systemBackground
-
-    let filterSelectionView = HGridView(numberOfColumns: 1)
-
-    filterSelectionView.setContents([])
 
     Mondrian.buildSubviews(on: view) {
 
@@ -38,13 +43,53 @@ final class EditViewController: CodeBasedViewController, ViewControllerFluidCont
           .padding(.horizontal, 8)
           .alignSelf(.fill)
 
-        filterSelectionView
+        presetSelectionView
           .viewBlock
           .height(80)
       }
       .container(respectingSafeAreaEdges: .all)
 
     }
+
+    let cellRegistration = DynamicContentListView<PreviewFilterPreset>.CellRegistration<PresetCell>
+      .init { cell, indexPath, item in
+        cell.setData(item)
+      }
+
+    presetSelectionView.setUp(
+      cellForItemAt: { collectionView, item, indexPath in
+        collectionView.dequeueConfiguredReusableCell(
+          using: cellRegistration,
+          for: indexPath,
+          item: item
+        )
+      },
+      didSelectItemAt: { [unowned self] preset in
+
+        editingStack.set {
+          $0.preset = preset.filter
+        }
+
+        print(preset)
+      }
+    )
+
+    editingStack.sinkState { [weak self] state in
+
+      guard let self = self else { return }
+
+      if let loadedState = state.mapIfPresent(\.loadedState) {
+
+        self.presetSelectionView.setContents(loadedState.previewFilterPresets)
+
+      } else {
+        let loadingState = state.map(\.loadingState)
+
+      }
+
+    }
+    .store(in: &subscriptions)
+
   }
 
   override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
@@ -56,6 +101,33 @@ final class EditViewController: CodeBasedViewController, ViewControllerFluidCont
 }
 
 extension EditViewController {
+
+  private final class PresetCell: UICollectionViewCell {
+
+    override init(
+      frame: CGRect
+    ) {
+      super.init(frame: frame)
+
+      Mondrian.buildSubviews(on: self) {
+        ZStackBlock {
+          UILabel()&>.do {
+            $0.text = "Hey"
+          }
+        }
+      }
+    }
+
+    required init?(
+      coder: NSCoder
+    ) {
+      fatalError("init(coder:) has not been implemented")
+    }
+
+    func setData(_ data: PreviewFilterPreset) {
+
+    }
+  }
 
   private static func makeFilterCell(onTap: @escaping () -> Void) -> UIView {
 
